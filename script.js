53eb01d741ff412e485f8dd1301d3a0f1a5ce069
MKZIN <mayckjuniordasilva19@gmail.com>
2024-12-18 19:00:34 -0300

    Atualizando arquivos do site

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 // Exemplo de mensagem no console
 console.log("Bem-vindo ao site da Cell Tech!");
+
+// Rolagem suave para as seções
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
